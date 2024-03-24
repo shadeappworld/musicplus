00600ced26a5da25e13b4f9260c7f4b4f9f57573
Shade Software <121112889+shadeappworld@users.noreply.github.com>
2024-03-24 19:50:48 +0200

    Add files via upload

---
+++ src/scripts/main.ts
@@ -0,0 +1,30 @@
+// import in order of site usage to minimize loading time
+import '../stylesheets/style.css';
+import './api';
+import './router';
+import './theme';
+import './search';
+import './audioEvents';
+import './library';
+import './superModal';
+import './queue';
+import './settings';
+import './list';
+import '../components/streamItem';
+import '../components/listItem';
+import '../components/toggleSwitch';
+import { registerSW } from 'virtual:pwa-register';
+
+const updater = document.createElement('update-prompt') as HTMLElement & { handleUpdate(): void };
+
+updater.handleUpdate = registerSW({
+  async onNeedRefresh() {
+    const { html, render } = await import('lit');
+    import('../components/updatePrompt').then(() => render(
+      html`<dialog id='changelog' open>
+      ${updater}</dialog>`,
+      document.body
+    ));
+  }
+});
+
